02e3b4ee141cfc2da0f3ecfeaa2f1bb9f741105c
yuto ichinose <yuto.0526.sano@gmail.com>
2022-05-25 15:21:36 +0900

    update: fetchが失敗した時の処理を追加、もう少し考えてみたい

--- js/list.js
+++ js/list.js
@@ -1,15 +1,31 @@
 'use strict'
 window.addEventListener('load', e => {
     
+    // fetctでファイルあるか試す関数いる？
+
     (async () => {
         // JSONファイル読み込み
-        const res = await fetch('./js/lib/md.json')
+        const option = {
+            method: 'GET'
+        }
+        // const res = await fetch('./js/lib/md.jsona', option)
+        const res = await fetch('./js/lib/md.json', option)
+        console.log(res);
+        if(!res.ok) { //パス間違えててデータ取得できなかった場合
+            alert('データ取得失敗')
+            return
+        }
         const json =await res.json()
         // console.log(json)
         // 要素生成
         const liFragment = document.createDocumentFragment()
         for (const i of json) {
-            // console.log(i)
+
+            const md = await fetch(`./md/${i}`)
+            // console.log(md);
+            if(!md.ok) { //パス間違えててデータ取得できなかった場合
+                continue //リンク間違えたaタグ作らないために飛ばす
+            }
             const liEle = document.createElement('li')
             const aEle = document.createElement('a')
             aEle.href = `./md/${i}`
